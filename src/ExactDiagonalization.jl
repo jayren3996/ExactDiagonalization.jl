64ba08e1d2aebaeb4b96a3c1e00d6f1a673ef2f0
JayRen <jieren3996@foxmail.com>
2020-09-13 15:10:45 +0800

    update

--- src/ExactDiagonalization.jl
+++ src/ExactDiagonalization.jl
@@ -15,5 +15,58 @@ include("Basis.jl")
 include("Operation.jl")
 include("Measure.jl")
 include("Spin.jl")
+#-----------------------------------------------------------------------------------------------------
+# Krylov space
+#-----------------------------------------------------------------------------------------------------
+function reducespace(
+    vs::AbstractMatrix; 
+    rtol::Real=1e-3
+)
+    mat = vs' * vs
+    e,v = eigh(mat)
+    pos = e .> rtol
+    ep = e[pos]
+    vp = v[:,pos]
+    nvs = vs * vp
+    for i=1:length(ep)
+        nvs[:,i] ./= sqrt(ep[i])
+    end
+    nvs
+end
+#-----------------------------------------------------------------------------------------------------
+function reducespace(
+    v::AbstractVector; 
+    rtol::Real=1e-3
+)
+    vs = reshape(v,:,1)
+    reducespace(vs, rtol=rtol)
+end
+#-----------------------------------------------------------------------------------------------------
+export krylovspace
+function krylovspace(
+    H, 
+    vs::AbstractMatrix; 
+    rtol::Real=1e-3
+)
+    r = rank(vs)
+    v = reducespace(hcat(vs, H * vs), rtol=rtol)
+    nr = size(v,2)
+    while nr > r
+        r = nr
+        v = reducespace(hcat(v, H * v), rtol=rtol)
+        nr = size(v,2)
+    end
+    v
+end
+#-----------------------------------------------------------------------------------------------------
+function krylovspace(
+    H, 
+    v::AbstractVector; 
+    rtol::Real=1e-3
+)
+    vs = reshape(v,:,1)
+    krylovspace(H, vs, rtol=rtol)
+end
+
 
 end # module ExactDiagonalization
